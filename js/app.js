380c8b01f6e08b1f2a4d0300187af36df94287cc
Ryan Geddes <rgeddes09@gmail.com>
2020-05-23 19:24:17 -0700

    scaffolding

---
+++ js/app.js
@@ -0,0 +1,60 @@
+//TODO: 
+//import excel of current offices
+
+
+
+
+//TODO: 
+//write logic to pull spreadsheet of offices from tableau
+
+
+
+
+//TODO: 
+// write logic to compare new list of offices to master list of offices and add new offices to spreadsheet
+
+
+
+
+//TODO: 
+//write logic to search salesforces for existing accounts for new offices
+// based on office name and contact info from reference feed
+
+
+
+
+//TODO: 
+//write logic to present a list of salesforce results for existing accounts for new offices
+
+
+
+
+//TODO: 
+//write logic to create new sf accounts for offices where no existing salesforce account is found
+
+
+
+
+//TODO: 
+//write logic to create new sf contact for missing listing in new account, based on ALT reference feed
+
+
+
+
+//TODO: 
+//write logic to draft email for missing listing, referencing:
+//correct property type and addressed to agent
+//include HTML link to missing Z listing
+// stretch goal: scrape zillow for screenshot of address
+
+
+
+
+//TODO: 
+//write logic to communicate with microsoft outlook and present a batch of emails for user's review to send
+// review:
+// https://blogs.perficient.com/2016/10/17/create-a-microsoft-outlook-email-using-javascript/
+// https://stackoverflow.com/questions/35229240/javascript-open-outlook-and-add-attachments-to-new-email
+
+
+
